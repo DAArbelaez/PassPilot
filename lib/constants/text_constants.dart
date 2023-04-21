8c4223fd94cdd858300f48549508cd4fe7f54a39
Diego Andres Arbelaez <diegoandresarbelaez@gmail.com>
2023-04-21 10:53:00 -0500

    Add text and ui constants

---
+++ lib/constants/text_constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextConstants {
+  static const kTitleStyleText = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const kSubTitleStyleText = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const kRegularStyleText = TextStyle(
+    fontSize: 16,
+  );
+}
